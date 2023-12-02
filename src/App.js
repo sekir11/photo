1eb086941385e0f723ec32e43f0f32884a9e5228
sekir11 <seki.ryohei0721@gmail.com>
2023-12-02 21:52:33 +0900

    feature: disabled swipe operation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Home from "./component/Home";
 import PhotoItem from "./component/PhotoItem";
 import { useState } from "react";
 
+(function () {
+  var c = document.getElementById("main");
+  c.addEventListener = null;
+})();
+
 function App() {
   const [recordList, setRecordList] = useState([]);
 
